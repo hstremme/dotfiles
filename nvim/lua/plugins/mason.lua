f66bff635f3c31ea3505d22fdb42d0ea0bbda749
stremme <h.stremme@posteo.de>
2024-11-12 15:31:07 +0100

    initial lsp setup with cmp and mason

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+return {
+  'williamboman/mason.nvim',
+  dependencies = {'williamboman/mason-lspconfig.nvim'},
+  config = function()
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup({})
+        end,
+      },
+    })
+  end
+}
