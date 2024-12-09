e0e280d06ee69ce012860858ff341c5b15839e31
Stremme <h.stremme@posteo.com>
2024-12-09 22:04:14 +0100

    enabled indentation, extra rule for php

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -9,3 +9,13 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.scrolloff = 8
+
+-- Set indentation specifically for PHP files
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "php",
+    callback = function()
+        vim.opt.tabstop = 4
+        vim.opt.softtabstop = 4
+        vim.opt.shiftwidth = 4
+    end,
+})
