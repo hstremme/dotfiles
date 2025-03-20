804e6623f3ef6d0704537d1ec01c40a88d9c6938
stremme <h.stremme@posteo.de>
2025-03-20 15:41:28 +0100

    added touble package (maybe no use case) and added mapping for diagnostics

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,3 +5,7 @@ vim.keymap.set("n", "<leader>pv", ":Ex<CR>", { desc = 'Back to browse' })
 
 -- Search highlight reset (No Highlight)
 vim.keymap.set("n", "<leader>nh", ":noh<CR>", { desc = '[n]o [h]ighlight' })
+
+-- Diagnostics Iteration
+vim.keymap.set("n", "<leader>dn", ":lua vim.diagnostic.goto_next() <CR>", { desc = '[n]ext [d]iagnostic' })
+vim.keymap.set("n", "<leader>dp", ":lua vim.diagnostic.goto_prev() <CR>", { desc = '[p]revious [d]iagnostic' })
